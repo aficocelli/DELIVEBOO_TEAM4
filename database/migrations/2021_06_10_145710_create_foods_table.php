089d066219f52566e771b27ac4d1024e3420ba4a
Lorena Spiller <lorenaspiller@gmail.com>
2021-06-10 17:02:19 +0200

    create migration
    restaurants, orders, types, users + update, foods

---
+++ database/migrations/2021_06_10_145710_create_foods_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('restaurant_id')->constrained()->onDelete('cascade');
+            $table->string('name_food', 150);
+            $table->string('ingredients');
+            $table->float('price', 5, 2);
+            $table->boolean('vegan')->default(0);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
