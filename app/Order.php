3c6256e28174aff59c4f775cb0dd40a021dc86fb
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-06-10 17:58:59 +0200

    relazioni tabelle

--- app/Order.php
+++ app/Order.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $guarded = [];
+
+    public function foods(){
+
+        return $this->belongsToMany('App\Food');
+
+    }
 }
