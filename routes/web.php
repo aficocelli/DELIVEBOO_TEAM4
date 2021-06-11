fb33eb2471400ff3bd1d8bc76d841efa6c7a4851
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-06-11 15:15:02 +0200

    aggiunte rotte foods

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+// Route::resource('/', 'FoodController');
+
+
+Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
+    Route::resource('foods', 'FoodController');
 });
 
 Auth::routes();
