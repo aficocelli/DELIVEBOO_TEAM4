43f3f4bdacea4deeac787e3088da1f262fc9b58e
francescapetra <francescalaurapetra@gmail.com>
2021-06-11 11:36:46 +0200

    food_seeder

--- app/Food.php
+++ app/Food.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Food extends Model
 {
+    public $table = "foods";
     protected $guarded = [];
 
     public function restaurant(){
---
+++ database/seeds/FoodsTableSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Food;
+
+class FoodsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $foods = [
+            [
+                'id' => 1,
+                'restaurant_id' => 2,
+                'name_food' => 'pizza',
+                'image' => 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.buttalapasta.it%2Fricette%2Fricetta-hamburger-americano-tradizionale%2F25325%2F&psig=AOvVaw1SyweUS3xqFYgbu6j6xV7-&ust=1623488307418000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjN_rGbj_ECFQAAAAAdAAAAABAD',
+                'ingredients' => 'acqua, farina, pomodoro, mozzarella, basilico',
+                'price' => 6.50,
+                'vegan' => 0,
+                'description' => 'una pizza margherita form da block, very buona',
+                'available' => 1
+            ],
+            [
+                'id' => 2,
+                'restaurant_id' => 3,
+                'name_food' => 'hamburger',
+                'image' => 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcookidoo.it%2Frecipes%2Frecipe%2Fit-IT%2Fr652170&psig=AOvVaw3xsJgv-0KenGLyO_DcSTb-&ust=1623488286374000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOiaraibj_ECFQAAAAAdAAAAABAD',
+                'ingredients' => 'pane, cipolla, lattuga, pomodoro, carne',
+                'price' => 4.50,
+                'vegan' => 0,
+                'description' => 'un panino very scrauso',
+                'available' => 1
+            ]
+        ];
+        foreach ($foods as $food) {
+            $newFood = new Food();
+            $newFood->id = $food['id'];
+            $newFood->restaurant_id = $food['restaurant_id'];
+            $newFood->name_food = $food['name_food'];
+            $newFood->image = $food['image'];
+            $newFood->ingredients = $food['ingredients'];
+            $newFood->price = $food['price'];
+            $newFood->vegan = $food['vegan'];
+            $newFood->description = $food['description'];
+            $newFood->available = $food['available'];
+            $newFood->save();
+        }
+    }
+}
