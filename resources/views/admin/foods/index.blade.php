816b6e5f8b7bb446dd0abf885b1728b313acedf7
francescapetra <francescalaurapetra@gmail.com>
2021-06-14 20:19:02 +0200

    link fontawesome

--- resources/views/admin/foods/index.blade.php
+++ resources/views/admin/foods/index.blade.php
@@ -1,10 +1,13 @@
-@extends('layouts.admin')
+@extends('layouts.app')
 
 @section('pageTitle')
     Lista di menu
 @endsection
-<a href="{{route('admin.foods.create')}}"><button type="button" class="btn btn-success"><i class="fas fa-pencil-alt"></i>Aggiungi un nuovo menu</button></a>
+
+
 @section('content')
+<div class="container-sm">
+<a href="{{route('admin.foods.create')}}"><button type="button" class="btn btn-success"><i class="fas fa-pencil-alt"></i>Aggiungi un nuovo menu</button></a>
         @foreach ($foods as $food)
         <h1>{{$food->name_food}}</h1>    
         <p>Disponibile: {{$food->available}}</p>
@@ -16,6 +19,6 @@
     <a href="{{route('admin.foods.edit', [ 'food' => $food->id ])}}"><button type="button" class="btn btn-success"><i class="fas fa-pencil-alt"></i></button></a>
     <a href="{{route('home')}}">
             <button type="button" class="btn btn-primary ">Home Dashboard</button>
-          </a>
+          </a></div>
 
 @endsection
