b8e075b89c28fa845e27a2c9de551a707fdfa67d
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-06-18 19:13:38 +0200

    installato braintree

--- app/Http/Controllers/Guest/OrderController.php
+++ app/Http/Controllers/Guest/OrderController.php
@@ -5,17 +5,38 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Order;
+use Braintree;
 
 class OrderController extends Controller
 {
-    public function showOrder(){
-        $orders = Order::all();
+    // public function showOrder(){
+    //     $orders = Order::all();
 
-        return view('guest.order', compact('orders'));
-    }
+    //     return view('guest.order', compact('orders'));
+    // }
 
-    public function storeOrder(){
+    // public function storeOrder(){
 
+    //     $gateway = new Braintree\Gateway([
+    //         'environment' => 'sandbox',
+    //         'merchantId' => '3f58gf44rjwx3cz4',
+    //         'publicKey' => 'xkyy5gnc8czp7f9z',
+    //         'privateKey' => 'bf10ce31d57cec4edd1505e025f84a77'
+    //     ]);
 
-    }
+    //     $clientToken = $gateway->clientToken()->generate();
+
+    //     $nonceFromTheClient = $_POST["payment_method_nonce"];
+
+    //     $result = $gateway->transaction()->sale([
+    //         'amount' => '10.00',
+    //         'paymentMethodNonce' => $nonceFromTheClient,
+    //         'deviceData' => $deviceDataFromTheClient,
+    //         'options' => [
+    //             'submitForSettlement' => True
+    //         ]
+    //     ]);
+    //     dd($nonceFromTheClient);
+    //     return view('guest.order.store', compact('clientToken'));
+    // }
 }
--- resources/views/guest/order.blade.php
+++ resources/views/guest/order.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
     <div class="container-sm">
@@ -14,5 +14,33 @@
         @endforeach
     </div>
 
-@endsection
+@endsection --}}
 
+<head>
+  <meta charset="utf-8">
+  <script src="https://js.braintreegateway.com/web/dropin/1.30.1/js/dropin.min.js"></script>
+</head>
+<body>
+  <!-- Step one: add an empty container to your page -->
+  <div id="dropin-container">
+
+    <h1>Qui va il carrello?</h1>
+
+  </div>
+
+  <script type="text/javascript">
+  // Step two: create a dropin instance using that container (or a string
+//   that functions as a query selector such as `#dropin-container`)
+
+    braintree.dropin.create({
+    container: document.getElementById('dropin-container'),
+  // ...plus remaining configuration
+        authorization: CLIENT_TOKEN_FROM_SERVER,
+        container: '#dropin-container'
+        
+    }, (error, dropinInstance) => {
+  // Use `dropinInstance` here
+  // Methods documented at https://braintree.github.io/braintree-web-drop-in/docs/current/Dropin.html
+    });
+  </script>
+</body>
