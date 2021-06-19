073ac161888a91f59bc3f7db623ce79e24985f46
Lorena Spiller <lorenaspiller@gmail.com>
2021-06-19 16:58:12 +0200

    prima parte implementazione show

--- resources/views/guest/show.blade.php
+++ resources/views/guest/show.blade.php
@@ -32,16 +32,16 @@
 
       </div>
     </div>
-    <div class="container mt-5">
 
+    <div class="container mt-5">
       <div class="restaurant-menu">
         <ul class="list-inline">
           @foreach ($foods as $food)
-              <li class="menu-item">
-                <p>{{$food->name_food}}</p>
-                <span><button @@click="incrementa">+</button></span> 
-                <span><button @@click="decrementa">-</button></span> 
-                <span> @{{ordine}} </span>
+              <li class="menu_item">
+                <p class="menu_text">{{$food->name_food}}</p>
+                <button class="btn_contatore mr-2 ml-2" @@click="incrementa"><i class="fas fa-plus-circle"></i></button>
+                <button class="btn_contatore mr-2" @@click="decrementa"><i class="fas fa-minus-circle"></i></button> 
+                <p class="menu_text">@{{ordine}}</p>
                 {{-- <form action="">
                   <span>
                     <select name="" id="" v-model="mainSelect" v-on:change="">
@@ -57,9 +57,54 @@
           @endforeach
         </ul>
       </div>
-      {{-- carrello --}}
-        
-      {{-- /carrello --}}
+      <section id="root">
+        <div class="container-cart">
+            {{-- corpo del carrello --}}
+            <div class="cart-body">
+              <div class="cart-header">
+                <h1 class="main__title">Dropfood</h1>
+                <div class="show__hide">X</div>
+              </div>
+              <div class="cart-main">
+                <div class="row row-names">
+                  <div class="col__name col-product">Prodotto</div>
+                  <div class="col__name col-price">Prezzo</div>
+                  <div class="col__name col-qnt">Quantità</div>
+                </div>
+
+                <div class="row row-operations">
+                  <div class="col__name col-product product_image">
+                    <img class="product__image" src="https://via.placeholder.com/150/0000FF/808080 ?Text=Digital.comC/O https://placeholder.com/"  alt="" />
+                    <p class="cart__par">Pizza</p>
+                  </div>
+                  
+                  <div class="col__name col-price col-numeric">@{{basePrice}}$</div>
+                  
+                  <div class="col__name col-qnt">
+                    <button class="qty qty-minus" @@click="takeOne">-</button>
+                    <input class="input_cart" type="numeric" value="" v-model="qty"/>
+                    <button class="qty qty-plus" @@click="addOne">+</button>
+                  </div>
+
+                </div>
+                <div class="row-total">
+                    <div class="total">
+                        <p>Total: @{{total}} $</p>
+                    </div>
+                </div>
+                {{-- bottoni --}}
+                <div class="actions">
+                <a href="{{route('guest.order.create')}}"><button class="cart__btn">Procedi al checkout</button></a>
+                <a href=""><button class="cart__btn " @@click="calc">Update cart</button></a>
+                </div>
+            </div>
+          </div>
+
+
+              
+            
+        </div>
+      </section>
     </div>
 
 
