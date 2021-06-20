7654da0ada9a27201c0fef7ed9514fa00c2ecab8
francescapetra <francescalaurapetra@gmail.com>
2021-06-20 18:05:25 +0200

    bottone home

--- resources/views/parts/guest/header-index.blade.php
+++ resources/views/parts/guest/header-index.blade.php
@@ -45,8 +45,8 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
-                                    <a class="dropdown-item" href="{{ route('home') }}">{{Auth::user()->name_restaurant}}</a>
                                 </div>
+                                <a class="dropdown-item" href="{{ route('home') }}">{{Auth::user()->name_restaurant}}</a>
                             </li>
                         @endguest
                             
