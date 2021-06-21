1445d141f5101e665a04c6a3dab1550b8c1e32e2
Filippo Garota <filogarota101@gmail.com>
2021-06-21 12:53:41 +0200

    local_storage

--- app/Http/Controllers/Guest/OrderController.php
+++ app/Http/Controllers/Guest/OrderController.php
@@ -25,26 +25,52 @@ class OrderController extends Controller
 
     public function storeOrder(Request $request){
 
-        // $foods = Food::all();
-
+        $foods = Food::all();
+        dd($foods);
         $data = $request->all();
+
         $data['total'] = 20;
 
         $newOrder = Order::create($data);
+
+        $gateway = new Braintree\Gateway([
+            'environment' => 'sandbox',
+            'merchantId' => '3f58gf44rjwx3cz4',
+            'publicKey' => 'xkyy5gnc8czp7f9z',
+            'privateKey' => 'bf10ce31d57cec4edd1505e025f84a77'
+        ]);
+
+        $result = $gateway->transaction()->sale([
+            'amount' => $data['total'],
+            'paymentMethodNonce' =>$request->payment_method_nonce,
+            'options' => [
+                'submitForSettlement' => True
+            ]
+        ]);
+
         // dd($newOrder);
-     
-        //$newOrder->foods()->attach($data['foods']);
+
+        $newOrder->foods()->attach($data['foods']);
 
         //Mail::to($newOrder->email_guest)->send(new Model($newOrder));
        
 
-        return redirect()->route('guest.order.payment',$newOrder);
+        return redirect()->route('guest.order.payment',$newOrder, compact('foods'));
     }
 
     public function successOrder(Order $order)
     {
-        
-        return view('guest.order.payment', compact('order'));
+        $gateway = new Braintree\Gateway([
+            'environment' => 'sandbox',
+            'merchantId' => '3f58gf44rjwx3cz4',
+            'publicKey' => 'xkyy5gnc8czp7f9z',
+            'privateKey' => 'bf10ce31d57cec4edd1505e025f84a77'
+        ]);
+
+        // pass $clientToken to your front-end
+        $clientToken = $gateway->clientToken()->generate();
+
+        return view('guest.order.payment', compact('order', 'clientToken'));
     }
     
 }
