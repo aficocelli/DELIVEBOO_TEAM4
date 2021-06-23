7384cf07106c09280f2181383a109d0fdb82be76
Filippo Garota <filogarota101@gmail.com>
2021-06-23 15:08:04 +0200

    Show fonts e colori

--- resources/views/parts/guest/header.blade.php
+++ resources/views/parts/guest/header.blade.php
@@ -4,8 +4,8 @@
             <div class="container">
                 <!-- sezione logo e login -->
                 <a class="navbar-brandm d-flex align-items-center logo-link" href="{{ url('/') }}">
-                    <img class="logo" src="{{asset('img-carousel/dropfood-logo-fuschia.svg')}}" alt="dropfood logo">
-                    <h1 class="dropfood">dropfood</h1>
+                    <img class="logo" src="{{asset('img-carousel/dropfood-logo-white.svg')}}" alt="dropfood logo">
+                    <h1 class="dropfood header-show">dropfood</h1>
                 </a>
                 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
