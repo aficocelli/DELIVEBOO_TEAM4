001c65bec667492bff4d8e71c8d5b22cb9a1f3a9
Filippo Garota <filogarota101@gmail.com>
2021-06-23 09:31:53 +0200

    immagine show

--- resources/views/guest/show.blade.php
+++ resources/views/guest/show.blade.php
@@ -10,7 +10,7 @@
 <div class="show-bg">
 
   <div class="wrapper-restaurant">
-      <div class="container">
+      <div class="container-info">
         <!-- info restaurant -->
         <div class="info-restaurant">
           <div class="info-text">
@@ -22,12 +22,12 @@
           @endforeach 
           </div>
           <div class="info-image">
-            <img src="{{$users->image_restaurant ? asset('storage/' . $users->image_restaurant) : 'http://lorempixel.com/400/300/food'}}" alt="{{$users->name_restaurant}}">
+            <img class="image__restaurant" src="{{$users->image_restaurant ? asset('storage/' . $users->image_restaurant) : 'http://lorempixel.com/400/300/food'}}" alt="{{$users->name_restaurant}}">
           </div>
-              
         </div>
-
+              
       </div>
+
     </div>
   
   {{-- /carrello --}}
