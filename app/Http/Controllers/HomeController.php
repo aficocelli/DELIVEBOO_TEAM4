f3ccdc6e24e29fc01036e41bde17dd4c7f8e3545
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-06-24 14:09:05 +0200

    bug order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -51,9 +51,9 @@ class HomeController extends Controller
         $orders = DB::table('orders')
         ->join('food_order', 'food_order.order_id', '=', 'orders.id')
         ->join('foods', 'foods.id', '=', 'food_order.food_id')
-        ->where('user_id', $user_id)->get();
+        ->where('user_id', $user_id )->get();
 
-      
+        dd($orders);
         
         // dd($orders);
     
