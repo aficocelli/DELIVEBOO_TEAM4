9fa8a613ab014b13674740f099a2068705d1ec56
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-06-24 10:42:15 +0200

    creata relazione tabelle foods orders users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use App\Http\Middleware\Authenticate;
 use App\User;
 use App\Food;
+use App\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -25,19 +27,38 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Order $order)
     {   
+
+        // $order = Order::all();
         
         $user = Auth::user();
 
         
 
         $user_id = Auth::id();
+
+        
        
         $foods = Food::where('user_id', $user_id)->get();
+
+
+        // $orders = DB::table('users')
+        // ->join('foods', 'users.id', '=', 'foods.user_id')
+        // ->join('orders', 'users.id', '=', 'orders.id')
+        // ->get();
+
+        $orders = DB::table('orders')
+        ->join('food_order', 'food_order.order_id', '=', 'orders.id')
+        ->join('foods', 'foods.id', '=', 'food_order.food_id')
+        ->where('user_id', $user_id)->get();
+        
+        
+        
+        
     
         $data = User::all()->where('id');
-
-        return view('admin.users.index', compact('data', 'user', 'foods'));
+        
+        return view('admin.users.index', compact('data', 'user', 'foods', 'orders'));
     }
 }
