810e6eac907250e9fbf2811673f40c62871a5c51
Lorena Spiller <lorenaspiller@gmail.com>
2021-06-25 11:11:07 +0200

    aggiunto logo orange e aggiustato layout success

--- resources/views/guest/order/success.blade.php
+++ resources/views/guest/order/success.blade.php
@@ -4,30 +4,27 @@
     Deliveboo | Pagamento
 @endsection
 
-<div id="root" class="container-sm" style="margin-top: 170px">
-  <h1 class="mt-3">Il tuo ordine è confermato</h1>
+<div id="root" class="container-sm info_success">
+  <h1 class="mt-3">Il tuo ordine è confermato <span class="text-success"><i class="fas fa-check-circle"></i></span></h1>
 
-  <h2>Dettaglio Ordine</h2>
-    <div class="container-sm mt-5 mb-5">     
-      
-      <h4><strong>Numero ordine:</strong> N. {{$order->id}}</h4>
-      <h4><strong>Nome cliente:</strong> </strong>{{$order->fullname_guest}}</h4>
-      <h4><strong>Recapito telefonico:</strong> </strong>{{$order->phone_guest}}</h4>
-      <h4><strong>Indirizzo:</strong> {{$order->address_guest}}</h4>
-      <h4><strong>Email:</strong> {{$order->email_guest}}</h4>
-      <h4><strong>Data ordine:</strong>{{ $order->created_at}}</h4>
-      <h4><strong>Tipo di consegna:</strong> {{$order->delivery_type}}</h4>
-      <h4><strong>Orario di consegna:</strong> {{date("H", strtotime('+4 hours'))}}:30</h4>
-      <h4><strong>Totale ordine:</strong> {{$order->total = number_format($order->total, 2)}} € </h4>
-      {{-- <h4>Totale: </h4><span id="totale_price" >@{{total}}</span> Euro --}}
-      
-</div>
+  <h3 class="mt-5">Dettaglio Ordine: </h3>
+  <div class="container-sm mt-3 mb-5">     
+    
+    <h4><strong>Numero ordine:</strong> N. {{$order->id}}</h4>
+    <h4><strong>Nome cliente:</strong> </strong>{{$order->fullname_guest}}</h4>
+    <h4><strong>Recapito telefonico:</strong> </strong>{{$order->phone_guest}}</h4>
+    <h4><strong>Indirizzo:</strong> {{$order->address_guest}}</h4>
+    <h4><strong>Email:</strong> {{$order->email_guest}}</h4>
+    <h4><strong>Data ordine:</strong> {{ $order->created_at}}</h4>
+    <h4><strong>Tipo di consegna:</strong> {{$order->delivery_type}}</h4>
+    <h4><strong>Orario di consegna:</strong> {{date("H", strtotime('+4 hours'))}}:30</h4>
+    <h4><strong>Totale ordine:</strong> {{$order->total = number_format($order->total, 2)}} € </h4>
+    {{-- <h4>Totale: </h4><span id="totale_price" >@{{total}}</span> Euro --}}
+  </div>
 
-
-
-<div class="mt-3">
-  <a href='{{route('guest.index')}}' class="btn btn-primary">Home</a>
-</div>
+  <div class="mt-3 text-center mb-3">
+    <a href='{{route('guest.index')}}' class="btn btn-outline-primary">Home</a>
+  </div>
 
 </div>
 
